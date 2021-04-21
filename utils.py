8ea76f48d06ed5bbecd98f5d53938ced5d2df57e
tharun369 <70960602+tharun369@users.noreply.github.com>
2021-04-21 21:00:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,9 +14,9 @@ class UTILS:
         self._app = app
         mail_settings = {
             "MAIL_SERVER": 'smtp.gmail.com',
-            "MAIL_PORT": 465,
-            "MAIL_USE_TLS": False,
-            "MAIL_USE_SSL": True,
+            "MAIL_PORT": 587,
+            "MAIL_USE_TLS": True,
+            "MAIL_USE_SSL": False,
             "MAIL_USERNAME": self._sender_email,
             "MAIL_PASSWORD": self._sender_pass
         }
