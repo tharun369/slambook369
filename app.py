6a4f438d7946155d79386fc1bf254114f72f50c6
tharun369 <70960602+tharun369@users.noreply.github.com>
2021-04-21 20:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from utils import UTILS
 
-
 app = Flask(__name__, template_folder='template')
-
-
 
 @app.route('/')
 def index():
@@ -13,17 +10,9 @@ def index():
 @app.route('/submit',methods = ['POST'])
 def submit():
     result = request.form
-    #dict = []
     dict2 = {}
     for k,v in result.items():
-        #dict.append([f"{k}:{v}"])
         dict2[k] = v
-    #print(dict, '\n', dict2)
-    #u = UTILS(username=dict[0][0].split(':')[-1], data=str(dict), app=app)
     u = UTILS(username=dict2['YourName'], data=dict2, app=app)
     u.run()
     return redirect(url_for('index'))
-
-#if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    #app.run(threaded=True, port=5000, debug=True)
