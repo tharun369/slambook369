2ec942d5db1299600620e124c8a89ec38e804115
tharun369 <70960602+tharun369@users.noreply.github.com>
2021-04-21 20:45:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request
 from utils import UTILS
-import json
+
 
 app = Flask(__name__, template_folder='template')
+
+
 
 @app.route('/')
 def index():
@@ -11,14 +13,17 @@ def index():
 @app.route('/submit',methods = ['POST'])
 def submit():
     result = request.form
-    dict = []
+    #dict = []
+    dict2 = {}
     for k,v in result.items():
-        dict.append([f"{k}:{v}"])
-    #print(dict, '\n')
-    u = UTILS(username=dict[0][1], data=dict)
+        #dict.append([f"{k}:{v}"])
+        dict2[k] = v
+    #print(dict, '\n', dict2)
+    #u = UTILS(username=dict[0][0].split(':')[-1], data=str(dict), app=app)
+    u = UTILS(username=dict2['YourName'], data=dict2, app=app)
     u.run()
     return redirect(url_for('index'))
 
 #if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-#    app.run(threaded=True, port=5000, debug=True)
+    #app.run(threaded=True, port=5000, debug=True)
--- utils.py
+++ utils.py
@@ -1,25 +1,45 @@
+from flask_mail import Mail, Message
 import json
-import os
-import smtplib
-
 
 class UTILS:
 
     _sender_email = 'tharuneshwar27@gmail.com'
     _sender_pass = 'tharun sendhil'
     _receiver_email = _sender_email
-    
-    def __init__(self, username, data):
+    _filename = 'slambookdata.json'
+
+    def __init__(self, username, data, app):
         self._data = data
         self._username = username
+        self._app = app
+        mail_settings = {
+            "MAIL_SERVER": 'smtp.gmail.com',
+            "MAIL_PORT": 465,
+            "MAIL_USE_TLS": False,
+            "MAIL_USE_SSL": True,
+            "MAIL_USERNAME": self._sender_email,
+            "MAIL_PASSWORD": self._sender_pass
+        }
+        self._app.config.update(mail_settings)
+        self.mail = Mail(self._app)
 
-    def _Mail(self):#message):
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        s.starttls()
-        s.login(self._sender_email, self._sender_pass)
-        text = str(self._data)
-        s.sendmail(self._sender_email, self._sender_email, text)
-        s.quit()
+    def _Store(self):
+        with open(self._filename, 'w') as jsn:
+            json.dump(self._data, jsn,indent=4)
+        jsn.close()
+        self._Mail()
+
+    def _Mail(self):
+        with self._app.app_context():
+            msg = Message()
+            msg.subject=f"Slam Book From ~ {self._username}"
+            msg.sender=self._sender_email
+            msg.recipients=[self._receiver_email]
+            msg.body=f'From {self._username}'
+            with self._app.open_resource(self._filename) as fp:
+                msg.attach(f"{self._filename}", "application/json", fp.read())
+            self.mail.send(msg)
+            print('mailed')
 
     def run(self):
-        self._Mail()
+        self._Store()
